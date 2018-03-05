cc38f3eea6c72947bf8107c667f45ed0f4e58eef
Matt Zuzolo <mattzuzolo@outlook.com>
2018-03-05 21:20:16 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,31 +1,31 @@
-class Person
+# class Person
   
-   def initialize(name)
-    @name = name
-  end
+#   def initialize(name)
+#     @name = name
+#   end
   
-  def name=(name)
-    name#= name.split
-  end
+#   def name=(name)
+#     name#= name.split
+#   end
   
-  def name
-    #{name}.strip
-  end  
-  
-  
+#   def name
+#     #{name}.strip
+#   end  
   
   
-  def initialize(job)
-    @job = job
-  end
-  
-  def job=(job)
-    job#= job.split
-  end
-  
-  def job
-    #{job}.strip
-  end  
   
   
-end
+#   def initialize(job)
+#     @job = job
+#   end
+  
+#   def job=(job)
+#     job#= job.split
+#   end
+  
+#   def job
+#     #{job}.strip
+#   end  
+  
+  
+# end
