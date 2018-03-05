fbe865bbe776dae94534da2f850dea26fc22e886
Matt Zuzolo <mattzuzolo@outlook.com>
2018-03-05 21:28:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,17 +18,13 @@ class Dog
   
   
   
-  # def initialize(breed)
-  #   @breed = breed
-  # end
+  def breed
+    @breed
+  end  
   
-  # def breed=(breed)
-  #   breed#= breed.split
-  # end
-  
-  # def breed
-  #   #{breed}.strip
-  # end  
+  def breed=(breed)
+    @breed = breed
+  end
   
   
 end
